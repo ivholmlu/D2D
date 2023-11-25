4e881b54f720c2ba8a14a447c9fbf2adf38bd17c
Dinussen Sivarasalingam <dinussen.sivarasalingam@nmbu.no>
2023-11-25 15:03:22 +0100

    fe : updated streamsync app with dropdown selector

--- streamsync/restaurants/main.py
+++ streamsync/restaurants/main.py
@@ -1,3 +1,4 @@
+from pyspark.sql import SparkSession
 import streamsync as ss
 import pandas as pd
 import plotly.express as px
@@ -5,14 +6,29 @@ import plotly.express as px
 # Its name starts with _, so this function won't be exposed
 
 # Import data
+import os
+os.environ["JAVA_HOME"] = "C:\Program Files\Java\jdk-20"
+# If you are using environments in Python, you can set the environment variables like this:
+# or similar to "/Users/kristian/miniforge3/envs/tf_M1/bin/python"
+os.environ["PYSPARK_PYTHON"] = "python"
+# os.environ["PYSPARK_DRIVER_PYTHON"] = "python" # or similar to "/Users/kristian/miniforge3/envs/tf_M1/bin/python"
+# Set the Hadoop version to the one you are using, e.g., none:
+os.environ["PYSPARK_HADOOP_VERSION"] = "without"
+
+spark = SparkSession.builder.appName('SparkCassandraApp').\
+    config('spark.jars.packages', 'com.datastax.spark:spark-cassandra-connector_2.12:3.4.1').\
+    config('spark.cassandra.connection.host', 'localhost').\
+    config('spark.sql.extensions', 'com.datastax.spark.connector.CassandraSparkExtensions').\
+    config('spark.sql.catalog.mycatalog', 'com.datastax.spark.connector.datasource.CassandraCatalog').\
+    config('spark.cassandra.connection.port', '9042').getOrCreate()
+# Some warnings are to be expected.
 
 
 def _get_main_df():
-    main_df = pd.read_csv('../../Data/summary_oneyear.csv')
-    # get unique name and lat lon
-    main_df = main_df.drop_duplicates(subset=['name'])
-    main_df = main_df[['name', 'lat', 'lon']]
-    main_df = main_df.reset_index(drop=True)
+    # main_df = pd.read_csv('../../Data/summary_oneyear.csv')
+    main_df = df_fish = spark.read.format("org.apache.spark.sql.cassandra").options(
+        table="fish_table_year", keyspace="fish_keyspace").load().toPandas()
+
     return main_df
 
 # Plot fishplant
@@ -20,6 +36,12 @@ def _get_main_df():
 
 def _update_plotly_fishplant(state):
     fishplant = state["fishplant_df"]
+
+    # get unique name and lat lon
+    fishplant = fishplant.drop_duplicates(subset=['name'])
+    fishplant = fishplant[['name', 'lat', 'lon']]
+    fishplant = fishplant.reset_index(drop=True)
+
     selected_num = state["selected_num"]
     sizes = [10]*len(fishplant)
     if selected_num != -1:
@@ -44,18 +66,49 @@ def _update_plotly_fishplant(state):
 
 def handle_click(state, payload):
     fishplant = state["fishplant_df"]
+
+    fishplant = fishplant.drop_duplicates(subset=['name'])
+    fishplant = fishplant[['name', 'lat', 'lon']]
+    fishplant = fishplant.reset_index(drop=True)
+
     state["selected"] = fishplant["name"].values[payload[0]["pointNumber"]]
     state["selected_num"] = payload[0]["pointNumber"]
     _update_plotly_fishplant(state)
 
 
+def handle_choice(state, payload):
+    fishplant = state["fishplant_df"]
+    fishplant = fishplant.drop_duplicates(subset=['name'])
+    fishplant = fishplant[['name']]
+    fishplant = fishplant.reset_index(drop=True)
+
+    state["selected"] = fishplant["name"].values[int(payload)]
+    state["selected_num"] = int(payload)
+    _update_plotly_fishplant(state)
+
+
+def _get_JSON(state):
+    fishplant = state["fishplant_df"]
+    # Create JSON with keys list(range(9)), and restaurant names as values
+    fishplant = fishplant.drop_duplicates(subset=['name'])
+    fishplant = fishplant[['name']]
+    fishplant = fishplant.reset_index(drop=True)
+    # sort alphabetically
+    fishplant = fishplant.sort_values(by=['name'])
+
+    my_json = dict(zip(list(range(len(fishplant))), fishplant["name"].values))
+    # Convert keys to strings
+    my_json = {str(key): value for key, value in my_json.items()}
+    state["fishplant_JSON"] = my_json
+
 # Initialise the state
+
 
 # "_my_private_element" won't be serialised or sent to the frontend,
 # because it starts with an underscore (not used here)
 initial_state = ss.init_state({
     "my_app": {
-        "title": "Restaurant selection"
+        "title": "Fishplant selection"
     },
     "_my_private_element": 1337,
     "selected": "Click to select",
@@ -64,3 +117,4 @@ initial_state = ss.init_state({
 })
 
 _update_plotly_fishplant(initial_state)
+_get_JSON(initial_state)
